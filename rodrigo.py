333bb01f8bec81b3ec382daba6f5588bd98b5e96
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-07-05 14:26:34 -0300

    setup testes no pycharm

---
+++ rodrigo.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
+from time import sleep
