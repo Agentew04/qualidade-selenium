d430cf214f858a811fd8204cd089e88d9fb7d5a7
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-07-07 18:00:54 -0300

    add testes
    arruma readme

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢰⡿⠋⠁⠀⠀⠈⠉⠙⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⣿⠇⠀⢀⣴⣶⡾⠿⠿⠿⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⣀⣀⣸⡿⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⣾⡟⠛⣿⡇⠀⠀⢸⣿⣿⣷⣤⣤⣤⣤⣶⣶⣿⠇⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀
-⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣆
-⢸⣿⠀⢸⣿⡇⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⣿⡇⣀⣠⣴⣾⣮⣝⠿⠿⠿⣻⡟
-⢸⣿⠀⠘⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠉⠀
-⠸⣿⠀⠀⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀
-⠀⠻⣷⣶⣿⣇⠀⠀⠀⢠⣼⣿⣿⣿⣿⣿⣿⣿⣛⣛⣻⠉⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢸⣿⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠙⠛⠛⠛⠋⠁⠀⠙⠻⠿⠟⠋⠑⠛⠋⠀⠀⠀⠀⠀
+# Testes Automatizados Web com Selenium
+
+Repositório com os códigos para testar a página web da
+[Universidade de Tampa](https://www.ut.edu) usando
+a biblioteca Selenium. Feito como trabalho
+final da disciplina de Qualidade de Software (ELC133)
+na Universidade Federal de Santa Maria. Desenvolvido por:
+
+* Rodrigo Appelt (@Agentew04)
+* Pedro Ramos (@PedroRamos360)
+* Jordano Xavier (@JordanoXavier)
+* Rafael Pregardier (@Pre9o)
--- rodrigo.py
+++ rodrigo.py
@@ -1,6 +1,153 @@
+from lib2to3.pgen2 import driver
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+
+
+def test_giving_button():
+    firefox_driver = webdriver.Firefox()
+    firefox_driver.get("https://www.ut.edu")
+    sleep(1)
+    givingButton = firefox_driver.find_element(By.XPATH, '//*[@id="page"]/header/div[1]/div[2]/nav/ul/li[5]/a')
+    assert givingButton is not None
+    print('Botao existe')
+    sleep(1)
+    assert givingButton.is_displayed()
+    print('Botao esta aparecendo')
+    sleep(1)
+    givingButton.click()
+    assert firefox_driver.current_url == "https://www.ut.edu/development-and-university-relations"
+    sleep(1)
+
+    sleep(10)
+    give_now_button = firefox_driver.find_element(By.XPATH, '//*[@id="content"]/div/div/div[2]/div/div/ul/li[2]/a')
+    assert give_now_button is not None
+    print('Botao Give Now existe')
+    assert give_now_button.is_displayed()
+    print('Botao Give Now aparece')
+    actions = ActionChains(firefox_driver)
+    actions.move_to_element(give_now_button).perform()
+    # WebDriverWait(driver, 20).until(
+    #     EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div/div[2]/div/div/ul/li[2]/a'))).click()
+    give_now_button.click()
+    assert firefox_driver.current_url == "https://www.ut.edu/development-and-university-relations/give-now"
+    print('Botao Give Now Manda para pagina certa')
+    firefox_driver.quit()
+
+
+def testa_formulario_doacao():
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.get('https://www.ut.edu/development-and-university-relations/give-now')
+    print('Esperando 10s para js atualizar')
+    sleep(10)
+    fifty_dollars_donation_button = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_gift_rdlstGivingLevels"]/div[2]/div/label')
+    fifty_dollars_donation_input = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_gift_rdGivingLevel2"]')
+    hundred_dollars_donation_button = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_gift_rdlstGivingLevels"]/div[4]/div/label')
+    hundred_dollars_donation_input = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_gift_rdGivingLevel4"]')
+    assert fifty_dollars_donation_button is not None
+    print('Botao de 50 dolares existe')
+    assert hundred_dollars_donation_button is not None
+    print('Botao de 100 dolares existe')
+
+    assert fifty_dollars_donation_button.is_displayed()
+    assert hundred_dollars_donation_button.is_displayed()
+    print('Dois botoes de quantidade aparecem')
+
+    hundred_dollars_donation_button.click()
+
+    assert (hundred_dollars_donation_button.get_attribute("class")
+            == "BBFormRadioLabel BBFormRadioLabelGivingLevel BBFormRadioLabelGivingLevelSelected")
+    assert (fifty_dollars_donation_button.get_attribute("class")
+            == "BBFormRadioLabel BBFormRadioLabelGivingLevel BBFormRadioLabelGivingLevelNotSelected")
+
+    sleep(3)
+    fifty_dollars_donation_button.click()
+    assert (hundred_dollars_donation_button.get_attribute("class")
+            == "BBFormRadioLabel BBFormRadioLabelGivingLevel BBFormRadioLabelGivingLevelNotSelected")
+    assert (fifty_dollars_donation_button.get_attribute("class")
+            == "BBFormRadioLabel BBFormRadioLabelGivingLevel BBFormRadioLabelGivingLevelSelected")
+    print('Clicar no outro radiobutton apaga o primeiro')
+    sleep(4)
+
+    primeiro_nome_textbox = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_billing_txtFirstName"]')
+    assert primeiro_nome_textbox is not None
+    assert primeiro_nome_textbox.is_displayed()
+    primeiro_nome_textbox.send_keys('Carlos')
+    sleep(1)
+
+    segundo_nome_textbox = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_billing_txtLastName"]')
+    assert segundo_nome_textbox is not None
+    assert segundo_nome_textbox.is_displayed()
+    segundo_nome_textbox.send_keys('Silva')
+    sleep(1)
+
+    email_textbox = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_billing_txtEmail"]')
+    assert email_textbox is not None
+    assert email_textbox.is_displayed()
+    email_textbox.send_keys('teste@gmail.com')
+    sleep(1)
+
+    country_combobox = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_billing_billingAddress_ddCountry"]')
+    assert country_combobox is not None
+    assert country_combobox.is_displayed()
+    countries_options = country_combobox.find_elements(By.XPATH, '*')
+    assert len(countries_options) == 5
+    print('Tem os 5 paises')
+    sleep(1)
+
+    endereco_textbox = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_billing_billingAddress_txtAddress"]')
+    assert endereco_textbox is not None
+    assert endereco_textbox.is_displayed()
+    endereco_textbox.send_keys('palacio do planalto')
+    sleep(1)
+
+    submit = chrome_driver.find_element(By.XPATH, '//*[@id="bboxdonation_btnSecurePayment"]')
+    assert submit is not None
+    assert submit.is_displayed()
+
+    ActionChains(chrome_driver).move_to_element(submit).perform()
+    assert submit.value_of_css_property("background-color") == "rgba(0, 178, 0, 1)"
+    assert submit.value_of_css_property("border") == "0 solid rgba(0, 178, 0, 1)"
+    sleep(1)
+    chrome_driver.quit()
+
+
+def test_expander_admissions():
+    firefox_driver = webdriver.Firefox()
+    firefox_driver.get("https://www.ut.edu")
+
+    admissions = firefox_driver.find_element(By.XPATH, '//*[@id="page"]/header/div[1]/div[3]/nav/ul/li[1]/a')
+    assert admissions is not None
+    assert admissions.is_displayed()
+    sleep(4)
+
+    ActionChains(firefox_driver).move_to_element(admissions).perform()
+    sleep(3)
+    parent = admissions.find_element(By.XPATH, './..')
+    ul = parent.find_element(By.XPATH, './ul')
+    lis = ul.find_elements(By.XPATH, '*')
+    sleep(3)
+    assert len(lis) == 9
+    print("todos elementos estao ali")
+
+    sleep(3)
+
+    for li in lis:
+        ActionChains(firefox_driver).move_to_element(li).perform()
+        sleep(1)
+        assert li.is_displayed()
+        print('Elemento visivel')
+        sleep(1)
+
+    lis[-1].click()
+    assert firefox_driver.current_url == 'https://www.ut.edu/admissions/spartan-ambassadors'
+
+
+    firefox_driver.quit()
+
