c40cfd24eaddfb2f894a3348c1d8781774fd4e41
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-07-08 17:18:24 -0300

    add test to events date

--- jordano.py
+++ jordano.py
@@ -4,8 +4,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from time import sleep
+from datetime import datetime
 
 service = Service()
+
+def test_events_date():
+    driver = webdriver.Chrome(service=service)
+    try:
+        driver.get("https://www.ut.edu/campus-life/calendar")
+        sleep(5)
+        
+        iframe = driver.find_element(By.ID, "trumba.spud.0.iframe")
+        driver.switch_to.frame(iframe)
+
+        eventsDate = driver.find_element(By.XPATH, f"//div[@class='twSimpleTableGroupHead']")
+        assert eventsDate.is_displayed()
+
+        expected_date = f"{datetime.now().strftime('%B')} {datetime.now().year}"
+        assert eventsDate.text == expected_date
+    finally:
+        driver.quit()
+
 
 def test_acessibility_button():
     driver = webdriver.Chrome(service=service)
@@ -20,3 +39,7 @@ def test_acessibility_button():
         
     finally:
         driver.quit()
+
+
+
+        
